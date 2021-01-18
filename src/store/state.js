552d5bfd46ea6096b27960c37c7ab689bd3223d6
Pratama Hasriyan <pratama@hasriyan.com>
2021-01-19 00:17:44 +0700

    another leftover

--- src/store/state.js
+++ src/store/state.js
@@ -4,14 +4,4 @@ export default {
   loadingStatus: false,
   processCompleted: false,
   errorMsg: {}
-  // all users
-  // users: [],
-  // // Unverified users
-  // unvUsers: [],
-  // // Single user data
-  // userData: [],
-  // // Single host data
-  // hostData: [],
-  // // pagination data
-  // pagination: {}
 }
